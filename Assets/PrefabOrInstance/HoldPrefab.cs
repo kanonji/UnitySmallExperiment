883acf12dcafd0c00f11d92d6545739cc91bc3a8
kanonji <kanonji@users.noreply.github.com>
2016-08-19 00:32:40 +0900

    Add PrefabOrInstance

---
+++ Assets/PrefabOrInstance/HoldPrefab.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Kanonji.SmallExperiment.PrefabOrInstance {
+
+	public class HoldPrefab : MonoBehaviour {
+
+		[SerializeField] protected GameObject prefab;
+
+		void Awake() {
+			Debug.Log("this.gameObject.name: " + gameObject.name);
+			Debug.Log("this.gameObject.activeInHierarchy: " + gameObject.activeInHierarchy);
+			Debug.Log("this.gameObject.GetInstanceID(): " + gameObject.GetInstanceID());
+			Debug.Log("this.gameObject.transform.position: " + gameObject.transform.position);
+			Debug.Log("this.prefab.name: " + prefab.name);
+			Debug.Log("this.prefab.activeInHierarchy: " + prefab.activeInHierarchy);
+			Debug.Log("this.prefab.GetInstanceID(): " + prefab.GetInstanceID());
+			Debug.Log("this.prefab.transform.position: " + prefab.transform.position);
+			Debug.Log("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+		}
+	}
+}
