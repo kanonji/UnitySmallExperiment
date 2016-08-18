4a46f7f72935425c609f7b0f92d65ca8fb8656e4
kanonji <kanonji@users.noreply.github.com>
2016-08-18 23:35:47 +0900

    Add VariableInitializer

---
+++ Assets/VariableInitializer/VariableInitialized.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Kanonji.SmallExperiment.VariableInitialized {
+
+	public class VariableInitialized : MonoBehaviour {
+
+		//[SerializeField] private float unityEngineRandomValue = UnityEngine.Random.value; //Got Error.
+		//[SerializeField] private int unityEngineRandomRange = UnityEngine.Random.Range(1, 101); //Got Error.
+		[SerializeField]
+		private float systemRandomRange = (new System.Random()).Next(100);
+		private static float staticUnityEngineRandomValue = UnityEngine.Random.value;
+		private static float staticSystemRandomRange = (new System.Random()).Next(100);
+
+		void Awake() {
+			Debug.Log("staticUnityEngineRandomValue: " + staticUnityEngineRandomValue);
+			Debug.Log("staticSystemRandomRange: " + staticSystemRandomRange);
+		}
+	}
+}
