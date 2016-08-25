d60e4889dc5d518dfb7469b6008a26b63e89f472
kanonji <kanonji@users.noreply.github.com>
2016-08-26 02:50:37 +0900

    Add PrefabValuesChangedInPlayMode

---
+++ Assets/PrefabValuesChangedInPlayMode/Instantiate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instantiate : MonoBehaviour {
+
+	[SerializeField] protected GameObject gameObject;
+
+	public void Invoke() {
+		Instantiate(gameObject);
+	}
+}
---
+++ Assets/PrefabValuesChangedInPlayMode/Move.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	[SerializeField] protected Vector3 direction;
+	[SerializeField] protected float speed;
+	
+	void Update () {
+		Vector3 moveVolume = direction * Time.deltaTime * speed;
+		transform.Translate(moveVolume);
+	}
+}
