88692799a4cb20e08c242fe3d13e697186a2f9c0
kanonji <kanonji@users.noreply.github.com>
2017-05-30 20:27:00 +0900

    Refine VariableInitializer

--- Assets/VariableInitializer/VariableInitialized.cs
+++ Assets/VariableInitializer/VariableInitialized.cs
@@ -3,16 +3,32 @@
 namespace Kanonji.SmallExperiment.VariableInitialized {
 
 	public class VariableInitialized : MonoBehaviour {
+		//UnityException: get_value is not allowed to be called from a MonoBehaviour constructor(or instance field initializer), call it in Awake or Start instead.Called from MonoBehaviour 'VariableInitialized'.
+		//See "Script Serialization" page in the Unity Manual for further details.
+		//[SerializeField] private float unityEngineRandomValue = UnityEngine.Random.value;
 
-		//[SerializeField] private float unityEngineRandomValue = UnityEngine.Random.value; //Got Error.
-		//[SerializeField] private int unityEngineRandomRange = UnityEngine.Random.Range(1, 101); //Got Error.
+		//UnityException: RandomRangeInt is not allowed to be called from a MonoBehaviour constructor (or instance field initializer), call it in Awake or Start instead. Called from MonoBehaviour 'VariableInitialized'.
+		//See "Script Serialization" page in the Unity Manual for further details.
+		//[SerializeField] private int unityEngineRandomRange = UnityEngine.Random.Range(1, 101);
+
 		[SerializeField] private float systemRandomRange = (new System.Random()).Next(100);
-		private static float staticUnityEngineRandomValue = UnityEngine.Random.value;
+
+		public float publicSystemRandomRange = (new System.Random()).Next(100);
+
+		//UnityException: get_value is not allowed to be called from a MonoBehaviour constructor (or instance field initializer), call it in Awake or Start instead. Called from MonoBehaviour 'VariableInitialized'.
+		//See "Script Serialization" page in the Unity Manual for further details.
+		//private static float staticUnityEngineRandomValue = UnityEngine.Random.value;
+
+		//UnityException: RandomRangeInt is not allowed to be called from a MonoBehaviour constructor(or instance field initializer), call it in Awake or Start instead.Called from MonoBehaviour 'VariableInitialized' on game object 'Prefab'.
+		//See "Script Serialization" page in the Unity Manual for further details.
+		//private static int staticUnityEngineRandomRange = UnityEngine.Random.Range(1, 101);
+
 		private static float staticSystemRandomRange = (new System.Random()).Next(100);
 
 		void Awake() {
-			Debug.Log("staticUnityEngineRandomValue: " + staticUnityEngineRandomValue);
-			Debug.Log("staticSystemRandomRange: " + staticSystemRandomRange);
+			Debug.Log(this.gameObject.name + ": systemRandomRange: " + systemRandomRange);
+			Debug.Log(this.gameObject.name + ": publicSystemRandomRange: " + publicSystemRandomRange);
+			Debug.Log(this.gameObject.name + ": staticSystemRandomRange: " + staticSystemRandomRange);
 		}
 	}
 }
